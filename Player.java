dc160ca54943f1eca16b9f740c3d8e26b162dd87
Kaan Uz <kaan.uz@ug.bilkent.edu.tr>
2025-03-01 14:13:06 +0300

    Updated Player.java
    Methods getAndRemoveTile, addTile, and isWinningHand are implemented.

--- Player.java
+++ Player.java
@@ -1,3 +1,6 @@
+/*
+    Author of ToDos in this class: Kaan Uz - 22402376
+*/
 public class Player {
     String playerName;
     Tile[] playerTiles;
@@ -14,7 +17,21 @@ public class Player {
      * TODO: removes and returns the tile in given index
      */
     public Tile getAndRemoveTile(int index) {
-        return null;
+        if(index < 0 || index >= this.numberOfTiles)
+        {
+            return null;
+        }
+        Tile willBeRemoved = this.playerTiles[index];
+
+        for(int i = index; i < this.numberOfTiles - 1; i++)
+        {
+            this.playerTiles[i] = this.playerTiles[i + 1];
+        }
+
+        this.playerTiles[this.numberOfTiles - 1] = null; // Removing the last reference 
+        this.numberOfTiles--;
+
+        return willBeRemoved;
     }
 
     /*
@@ -23,8 +40,23 @@ public class Player {
      * make sure playerTiles are not more than 15 at any time
      */
     public void addTile(Tile t) {
+        if(this.numberOfTiles >= 15)
+        {
+            System.out.println("Maximum tile limit reached, cannot add a new tile!");
+        }
+        else
+        {
+            int i = this.numberOfTiles - 1;
+            while(i >= 0 && this.playerTiles[i].compareTo(t) > 0)
+            {
+                this.playerTiles[i + 1] = this.playerTiles[i];
+                i--;
+            }
 
-    }
+            this.playerTiles[i + 1] = t;
+            this.numberOfTiles++;
+        }
+   }
 
     /*
      * TODO: checks if this player's hand satisfies the winning condition
@@ -34,7 +66,20 @@ public class Player {
      * @return
      */
     public boolean isWinningHand() {
-        return false;
+        int i = 0; int numberOfChains = 0;
+        while(i < this.numberOfTiles - 3)
+        {
+            if(this.playerTiles[i].compareTo(this.playerTiles[i + 1]) == 0 && this.playerTiles[i].compareTo(this.playerTiles[i + 2]) == 0 && this.playerTiles[i].compareTo(this.playerTiles[i + 3] == 0))
+            {
+                numberOfChains++;
+                i += 4;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return numberOfChains >= 3;
     }
 
     public int findPositionOfTile(Tile t) {
